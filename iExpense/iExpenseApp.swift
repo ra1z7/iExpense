3bb790bdbb36e73476dace0c52567b325d95e039
Purnaman Rai <purnamaanrai7@gmail.com>
2025-08-30 16:02:02 +0545

    Initial Commit

---
+++ iExpense/iExpenseApp.swift
@@ -0,0 +1,17 @@
+//
+//  iExpenseApp.swift
+//  iExpense
+//
+//  Created by Purnaman Rai (College) on 30/08/2025.
+//
+
+import SwiftUI
+
+@main
+struct iExpenseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
