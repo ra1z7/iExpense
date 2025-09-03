a1e5ea1270bfde1c6933de9004e07ce73f65c969
ra1z <purnamaanrai7@gmail.com>
2025-09-03 08:39:10 +0545

    Build a list we can delete from

--- iExpense/ContentView.swift
+++ iExpense/ContentView.swift
@@ -7,15 +7,41 @@
 
 import SwiftUI
 
+struct ExpenseItem {
+    let name: String
+    let type: String
+    let amount: Double
+}
+
+@Observable
+class Expenses { // we can make this class load and save itself seamlessly later
+    var items = [ExpenseItem]()
+}
+
+// now we have a struct to represent a single item of expense, and a class to store an array of all those items.
+
 struct ContentView: View {
+    @State private var expenses = Expenses()
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        NavigationStack {
+            List {
+                ForEach(expenses.items, id: \.name) { item in
+                    Text(item.name)
+                }
+                .onDelete(perform: removeItems)
+            }
+            .navigationTitle("iExpense")
+            .toolbar {
+                Button("Add Expense", systemImage: "plus") {
+                    expenses.items.append(ExpenseItem(name: "Test", type: "Personal", amount: 5.99))
+                }
+            }
         }
-        .padding()
+    }
+    
+    func removeItems(at offsets: IndexSet) {
+        expenses.items.remove(atOffsets: offsets)
     }
 }
 
